26923f654b4b82c8a67a4c4c449b83d8f5f66261
James Pacheco <jamesrp13@gmail.com>
2022-09-29 19:03:40 -0600

    Add solution to submit movie transaction

--- components/Form.tsx
+++ components/Form.tsx
@@ -14,6 +14,12 @@ import {
   Textarea,
   Switch,
 } from "@chakra-ui/react"
+import { useWorkspace } from "../context/Anchor"
+import { useWallet } from "@solana/wallet-adapter-react"
+import { findProgramAddressSync } from "@project-serum/anchor/dist/cjs/utils/pubkey"
+import { getAssociatedTokenAddressSync } from "@solana/spl-token"
+import * as anchor from "@project-serum/anchor"
+import InstructionNamespaceFactory from "@project-serum/anchor/dist/cjs/program/namespace/instruction"
 
 export const Form: FC = () => {
   const [title, setTitle] = useState("")
@@ -21,7 +27,53 @@ export const Form: FC = () => {
   const [description, setDescription] = useState("")
   const [toggle, setToggle] = useState(true)
 
-  const handleSubmit = async (event: any) => {}
+  const workspace = useWorkspace()
+  const { publicKey, sendTransaction } = useWallet()
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault()
+
+    if (!publicKey || !workspace.program || !workspace.connection) {
+      alert("Please connect your wallet")
+      return
+    }
+
+    const [mintPda] = findProgramAddressSync(
+      [Buffer.from("mint")],
+      workspace.program.programId
+    )
+
+    const tokenAddress = getAssociatedTokenAddressSync(mintPda, publicKey)
+
+    const transaction = new anchor.web3.Transaction()
+
+    if (toggle) {
+      const ix = await workspace.program.methods
+        .addMovieReview(title, description, rating)
+        .accounts({
+          tokenAccount: tokenAddress,
+        })
+        .instruction()
+
+      transaction.add(ix)
+    } else {
+      const ix = await workspace.program.methods
+        .updateMovieReview(title, description, rating)
+        .instruction()
+
+      transaction.add(ix)
+    }
+
+    try {
+      let signature = await sendTransaction(transaction, workspace.connection)
+      console.log(
+        `Transaction submitted: https://explorer.solana.com/tx/${signature}?cluster=devnet`
+      )
+    } catch (e) {
+      console.log("Error:", JSON.stringify(e))
+      alert(JSON.stringify(e))
+    }
+  }
 
   return (
     <Box
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import { ChakraProvider } from "@chakra-ui/react"
 import WalletContextProvider from "../context/WalletContextProvider"
+import { WorkspaceProvider } from "../context/Anchor"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <WalletContextProvider>
       <ChakraProvider>
-        <Component {...pageProps} />
+        <WorkspaceProvider>
+          <Component {...pageProps} />
+        </WorkspaceProvider>
       </ChakraProvider>
     </WalletContextProvider>
   )
